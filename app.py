805378d4ea58d2ec988a491cbea1673e15613532
Jakwanul-Safin <js5142@columbia.edu>
2021-12-04 01:17:23 -0500

    replaced requirements library with async implementation

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ import logging
 import requests
 from dataclasses import dataclass
 
+import time
+
 import asyncio
+import aiohttp
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -21,10 +24,11 @@ portfolio_api_path = "https://sldnph4bq3.execute-api.us-east-1.amazonaws.com/bet
 
 #  { “user_id”: “102", “ticker”: “ABC”, “quantity”: 10, “price”: 50.0, “transaction_type”: “BUY” }
 
+
 class Task(Protocol):
     async def do(self):
         ...
-    
+
     async def undo(self):
         ...
 
@@ -45,15 +49,32 @@ class AdjustMoneyManagement:
         user_money_path = f"{money_api_path}money/{self.user_id}"
         method = "addition" if self.amount >= 0 else "deduction"
         body = {"method": method, "money_amount": str(abs(self.amount))}
-        
-        return requests.put(user_money_path, json = body)
+
+        return requests.put(user_money_path, json=body)
 
     async def undo(self):
         user_money_path = f"{money_api_path}money/{self.user_id}"
         method = "addition" if self.amount <= 0 else "deduction"
         body = {"method": method, "money_amount": str(abs(self.amount))}
-        
-        return requests.put(user_money_path, json = body)
+
+        return requests.put(user_money_path, json=body)
+
+    async def do_async(self, session):
+        user_money_path = f"{money_api_path}money/{self.user_id}"
+        method = "addition" if self.amount >= 0 else "deduction"
+        body = {"method": method, "money_amount": str(abs(self.amount))}
+
+        async with session.put(user_money_path, json=body) as resp:
+            return resp.ok
+
+    async def undo_async(self, session):
+        user_money_path = f"{money_api_path}money/{self.user_id}"
+        method = "addition" if self.amount <= 0 else "deduction"
+        body = {"method": method, "money_amount": str(abs(self.amount))}
+
+        async with session.put(user_money_path, json=body) as resp:
+            return await resp.ok
+
 
 @dataclass
 class AdjustInvestmentPortfolio:
@@ -67,48 +88,117 @@ class AdjustInvestmentPortfolio:
 
     async def do(self):
         action = "sell" if self.quantity >= 0 else "buy"
-        investment_portfolio_path = f'{portfolio_api_path}/api/{action}/{self.user_id}'
-        body = {"user_id": self.user_id, "ticker": self.ticker, "quantity": self.quantity}
-        return requests.post(investment_portfolio_path, json = body)
+        investment_portfolio_path = f"{portfolio_api_path}/api/{action}/{self.user_id}"
+        body = {
+            "user_id": self.user_id,
+            "ticker": self.ticker,
+            "quantity": self.quantity,
+        }
+        return requests.post(investment_portfolio_path, json=body)
 
     async def undo(self):
         action = "buy" if self.quantity >= 0 else "sell"
-        investment_portfolio_path = f'{portfolio_api_path}/api/{action}/{self.user_id}'
-        body = {"user_id": self.user_id, "ticker": self.ticker, "quantity": self.quantity}
-        return requests.post(investment_portfolio_path, json = body)
+        investment_portfolio_path = f"{portfolio_api_path}/api/{action}/{self.user_id}"
+        body = {
+            "user_id": self.user_id,
+            "ticker": self.ticker,
+            "quantity": self.quantity,
+        }
+        return requests.post(investment_portfolio_path, json=body)
+
+    async def do_async(self, session):
+        action = "sell" if self.quantity >= 0 else "buy"
+        investment_portfolio_path = f"{portfolio_api_path}/api/{action}/{self.user_id}"
+        body = {
+            "user_id": self.user_id,
+            "ticker": self.ticker,
+            "quantity": self.quantity,
+        }
+
+        async with session.post(investment_portfolio_path, json=body) as resp:
+            return resp.ok
+
+    async def undo_async(self, session):
+        action = "buy" if self.quantity >= 0 else "sell"
+        investment_portfolio_path = f"{portfolio_api_path}/api/{action}/{self.user_id}"
+        body = {
+            "user_id": self.user_id,
+            "ticker": self.ticker,
+            "quantity": self.quantity,
+        }
+
+        async with session.post(investment_portfolio_path, json=body) as resp:
+            return resp.ok
+
+async def execute_pipeline_async(pipeline):
+    async with aiohttp.ClientSession() as session:
+        responses = await asyncio.gather(
+            *[asyncio.ensure_future(pipe.do_async(session)) for pipe in pipeline]
+        )
+        if all(resp for resp in responses):
+            return 1, []
+        await asyncio.gather(
+            *[
+                asyncio.ensure_future(pipe.undo_async(session))
+                for pipe, resp in zip(pipeline, responses)
+                if resp
+            ]
+        )
+        failed_pipes = [pipe.name for pipe, resp in zip(pipeline, responses) if not resp]
+        return 0, failed_pipes
+
 
 async def execute_pipeline(pipeline):
     responses = await asyncio.gather(*[pipe.do() for pipe in pipeline])
     if all(resp.ok for resp in responses):
         return 1, []
-    await asyncio.gather(*[pipe.undo() for pipe, resp in zip(pipeline, responses) if resp.ok])
+    await asyncio.gather(
+        *[pipe.undo() for pipe, resp in zip(pipeline, responses) if resp.ok]
+    )
     failed_pipes = [pipe.name for pipe, resp in zip(pipeline, responses) if not resp.ok]
     return 0, failed_pipes
 
 
-@app.route('/stockTransaction', methods=['POST'])
+@app.route("/stockTransaction", methods=["POST"])
 def transaction():
-    if request.method == 'POST':
+    start = time.perf_counter()
+    if request.method == "POST":
         # Gets stock's price and money, verifies transaction is feasible
 
         input = request.get_json()
         pipeline = []
-        if input['transaction_type'] == 'BUY':
+        if input["transaction_type"] == "BUY":
             pipeline = [
-                AdjustMoneyManagement(input['user_id'], -input["price"] * input["quantity"]),
-                AdjustInvestmentPortfolio(input["user_id"], input["ticker"], input["quantity"])
-            ]
-        elif input['transaction_type'] == 'SELL':
+                AdjustMoneyManagement(
+                    input["user_id"], -input["price"] * input["quantity"]
+                ),
+                AdjustInvestmentPortfolio(
+                    input["user_id"], input["ticker"], input["quantity"]
+                ),
+            ] * 20
+        elif input["transaction_type"] == "SELL":
             pipeline = [
-                AdjustMoneyManagement(input['user_id'], input["price"] * input["quantity"]),
-                AdjustInvestmentPortfolio(input["user_id"], input["ticker"], -input["quantity"])
-            ]
+                AdjustMoneyManagement(
+                    input["user_id"], input["price"] * input["quantity"]
+                ),
+                AdjustInvestmentPortfolio(
+                    input["user_id"], input["ticker"], -input["quantity"]
+                ),
+            ] * 20
 
-        res = asyncio.run(execute_pipeline(pipeline))
+        res = asyncio.run(execute_pipeline_async(pipeline))
+
+        print("Executed in time ", time.perf_counter() - start)
         if res[0]:
-            return Response(json.dumps({"success": 1}), status=201, content_type="application/json")
-        return Response(json.dumps({"error": f"{', '.join(res[1])} failed"}), status=201, content_type="application/json")
+            return Response(
+                json.dumps({"success": 1}), status=201, content_type="application/json"
+            )
+        return Response(
+            json.dumps({"error": f"{', '.join(res[1])} failed"}),
+            status=201,
+            content_type="application/json",
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
